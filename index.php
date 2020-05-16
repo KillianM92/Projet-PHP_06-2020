f35648e77e151be90f6332adbddf9cf6af008973
KillianM92 <killian.moutinard@ynov.com>
2020-05-16 18:24:17 +0200

    Ajout du fichier "awards.inc.php"
    Déplacement de la section "Diplômes & Certifications" dans le fichier awards.inc.php afin de rendre la page index.php plus lisible avec moins de contenu. + MàJ du fichier script.inc.php

--- index.php
+++ index.php
@@ -11,39 +11,9 @@
     <?php require_once("inc/skills.inc.php"); ?>
 
     <?php require_once("inc/interests.inc.php"); ?>
-    
 
-    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="awards">
-      <div class="w-100">
-        <h2 class="mb-5">Diplômes &amp; Certifications</h2>
-        <ul class="fa-ul mb-0">
-          <li>
-            <i class="fa-li fa fa-trophy text-warning"></i>
-            Permis de conduire Moto A2</li>
-          <li>
-            <i class="fa-li fa fa-trophy text-warning"></i>
-            Baccaulauréat Général Scientifique Mention Européenne Option Informatique Mention Assez Bien</li>
-          <li>
-            <i class="fa-li fa fa-trophy text-warning"></i>
-            Cambridge English Certificate Niveau B2 obtenu en Mars 2019</li>
-          <li>
-            <i class="fa-li fa fa-trophy text-warning"></i>
-            Cambridge English Certificate Niveau B1 obtenu en Mars 2017</li>
-          <li>
-            <i class="fa-li fa fa-trophy text-warning"></i>
-            Brevet des Collèges Option Européenne Mention Bien
-            </li>
-          <li>
-            <i class="fa-li fa fa-trophy text-warning"></i>
-            ASSR 1 & 2</li>
-        </ul>
-      </div>
-    </section>
+    <?php require_once("inc/awards.inc.php"); ?>
 
   </div>
 
 <?php include("inc/script.inc.php"); ?>
-
-</body>
-
-</html>

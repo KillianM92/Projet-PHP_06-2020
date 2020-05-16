34e3888279b85cab36a30e15ca61c513310fb7c2
KillianM92 <killian.moutinard@ynov.com>
2020-05-16 18:10:10 +0200

    Ajout du fichier "about.inc.php"
    Déplacement de la section "A propos" dans le fichier about.inc.php afin de rendre la page index.php plus lisible avec moins de contenu.

--- index.php
+++ index.php
@@ -2,33 +2,8 @@
 
   <div class="container-fluid p-0">
 
-    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
-      <div class="w-100">
-        <h1 class="mb-0">Killian
-          <span class="text-primary">Moutinard</span>
-        </h1>
-        <div class="subheading mb-5">6 rue de la fontaine Henri IV · Chaville, IDF, 92370 · +33 6 68 74 20 72 ·
-          <a href="mailto:name@email.com">killian.moutinard@ynov.com</a>
-        </div>
-        <p class="lead mb-5">Etudiant en première année d'informatique chez YNOV.</p>
-        <div class="social-icons">
-          <a href="https://github.com/KillianM92">
-            <i class="fab fa-github"></i>
-          </a>
-          <a href="https://twitter.com/K_Moutinard12">
-            <i class="fab fa-twitter"></i>
-          </a>
-          <a href="https://www.facebook.com/killian.moutinard">
-            <i class="fab fa-facebook-f"></i>
-          </a>
-          <a href="https://www.instagram.com/k.moutinard12/?hl=fr">
-            <i class="fab fa-instagram"></i>
-          </a>
-        </div>
-      </div>
-    </section>
+    <?php require_once("inc/about.inc.php"); ?>
 
-    <hr class="m-0">
 
     <section class="resume-section p-3 p-lg-5 d-flex justify-content-center" id="experience">
       <div class="w-100">
@@ -253,7 +228,7 @@
 
   </div>
 
-  <?php include("inc/script.inc.php"); ?>
+<?php include("inc/script.inc.php"); ?>
 
 </body>
 

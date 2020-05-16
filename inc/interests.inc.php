c45b66bdeea5007fee1b43d856dca7db2a397b7f
KillianM92 <killian.moutinard@ynov.com>
2020-05-16 18:20:05 +0200

    Ajout du fichier "interests.inc.php"
    Déplacement de la section "Centres d'intérêts" dans le fichier interests.inc.php afin de rendre la page index.php plus lisible avec moins de contenu.

---
+++ inc/interests.inc.php
@@ -0,0 +1,9 @@
+<section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="interests">
+    <div class="w-100">
+        <h2 class="mb-5">Centres d'intérêt</h2>
+        <p>En dehors de mes études dans le milieu de l'informatique, je passe pratiquement tout mon temps libre à faire du sport dans le domaine du basketball et de la musculation. J'aime beaucoup passer du temps avec mes amis, sortir pour se balader ou bien aller à des concerts et festivals.</p>
+        <p class="mb-0">Lorsque je suis chez moi, je regarde beaucoup de films et de séries fantastiques et d'horreur mais je ne rate pas l'occasion de regarder des matchs de différents sports. Le reste de mon temps libre est consacré aux jeux vidéos en ligne avec mes amis  mais aussi à me renseigner et tenter de découvrir les nouvelles technologies.</p>
+    </div>
+</section>
+
+<hr class="m-0">

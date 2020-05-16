115304993bde47ad06d9137b98fd5cfb7efd7b3b
KillianM92 <killian.moutinard@ynov.com>
2020-05-16 18:15:21 +0200

    Ajout du fichier "education.inc.php"
    Déplacement de la section "Formation" dans le fichier education.inc.php afin de rendre la page index.php plus lisible avec moins de contenu.

--- index.php
+++ index.php
@@ -6,41 +6,7 @@
 
     <?php require_once("inc/experience.inc.php"); ?>
 
-    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="education">
-      <div class="w-100">
-        <h2 class="mb-5">Formation</h2>
-
-        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-          <div class="resume-content">
-            <h3 class="mb-0">Lycée Jean-Pierre Vernant | Sèvres (92310)</h3>
-            <div class="subheading mb-3">Baccaulauréat Scientifique Option Informatique</div>
-            <div>Seconde Générale Section Européeenne Option Informatique & Création Numérique (2016/2017)</div>
-            <div>Première Scientifique Section Européenne (2017/2018)</div>
-            <div>Terminale Scientifique Section Européenne Option ISN (2018/2019)</div>
-          </div>
-          <div class="resume-date text-md-right">
-            <span class="text-primary">Septembre 2016 - Juin 2019</span>
-          </div>
-        </div>
-
-        <div class="resume-item d-flex flex-column flex-md-row justify-content-between">
-          <div class="resume-content">
-            <h3 class="mb-0">Paris YNOV Campus</h3>
-            <div class="subheading mb-3">Ingésup informatique</div>
-            <p>Bachelor 1 (2019/2020)</p>
-            <div>Développement Logiciels</div>
-            <div>Infrastructure & Système d'Informations</div>
-            <div>Technologies web & BDD </div>
-          </div>
-          <div class="resume-date text-md-right">
-            <span class="text-primary">Septembre 2019 - Aujourd'hui</span>
-          </div>
-        </div>
-
-      </div>
-    </section>
-
-    <hr class="m-0">
+    <?php require_once("inc/education.inc.php"); ?>
 
     <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="skills">
       <div class="w-100">

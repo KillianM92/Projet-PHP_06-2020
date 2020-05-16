47ebcb2f85c3b2d7574fc37264543e65ce340b35
KillianM92 <killian.moutinard@ynov.com>
2020-05-16 17:27:17 +0200

    Update de la rubrique "Diplômes"

--- index.php
+++ index.php
@@ -285,34 +285,27 @@
 
     <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="awards">
       <div class="w-100">
-        <h2 class="mb-5">Awards &amp; Certifications</h2>
+        <h2 class="mb-5">Diplômes &amp; Certifications</h2>
         <ul class="fa-ul mb-0">
           <li>
             <i class="fa-li fa fa-trophy text-warning"></i>
-            Google Analytics Certified Developer</li>
+            Permis de conduire Moto A2</li>
           <li>
             <i class="fa-li fa fa-trophy text-warning"></i>
-            Mobile Web Specialist - Google Certification</li>
+            Baccaulauréat Général Scientifique Mention Européenne Option Informatique Mention Assez Bien</li>
           <li>
             <i class="fa-li fa fa-trophy text-warning"></i>
-            1<sup>st</sup>
-            Place - University of Colorado Boulder - Emerging Tech Competition 2009</li>
+            Cambridge English Certificate Niveau B2 obtenu en Mars 2019</li>
           <li>
             <i class="fa-li fa fa-trophy text-warning"></i>
-            1<sup>st</sup>
-            Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)</li>
+            Cambridge English Certificate Niveau B1 obtenu en Mars 2017</li>
           <li>
             <i class="fa-li fa fa-trophy text-warning"></i>
-            2<sup>nd</sup>
-            Place - University of Colorado Boulder - Emerging Tech Competition 2008</li>
+            Brevet des Collèges Option Européenne Mention Bien
+            </li>
           <li>
             <i class="fa-li fa fa-trophy text-warning"></i>
-            1<sup>st</sup>
-            Place - James Buchanan High School - Hackathon 2006</li>
-          <li>
-            <i class="fa-li fa fa-trophy text-warning"></i>
-            3<sup>rd</sup>
-            Place - James Buchanan High School - Hackathon 2005</li>
+            ASSR 1 & 2</li>
         </ul>
       </div>
     </section>

7d6dfee5973561304f5b99f001dae3a5b27666df
KillianM92 <killian.moutinard@ynov.com>
2020-05-16 18:17:40 +0200

    Ajout du fichier "skills.inc.php"
    Déplacement de la section "Compétences" dans le fichier skills.inc.php afin de rendre la page index.php plus lisible avec moins de contenu.

--- index.php
+++ index.php
@@ -8,66 +8,7 @@
 
     <?php require_once("inc/education.inc.php"); ?>
 
-    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="skills">
-      <div class="w-100">
-        <h2 class="mb-5">Compétences</h2>
-
-        <div class="subheading mb-3">Langages de programmation &amp; Outils techniques</div>
-        <ul class="list-inline dev-icons">
-          <li class="list-inline-item">
-            <i class="fab fa-html5"></i>
-          </li>
-          <li class="list-inline-item">
-            <i class="fab fa-css3-alt"></i>
-          </li>
-          <li class="list-inline-item">
-            <i class="fab fa-js-square"></i>
-          </li>
-          <li class="list-inline-item">
-            <i class="fab fa-php"></i>
-          </li>
-          <li class="list-inline-item">
-            <i class="fab fa-windows"></i>
-          </li>
-          <li class="list-inline-item">
-            <i class="fab fa-linux"></i>
-          </li>
-          <li class="list-inline-item">
-            <i class="fab fa-git"></i>
-          </li>
-          <li class="list-inline-item">
-            <i class="fab fa-python"></i>
-          </li>
-          <li class="list-inline-item">
-            <i class="fab fa-c++"></i>
-          </li>
-        </ul>
-
-        <div class="subheading mb-3">Compétences transverses</div>
-        <ul class="fa-ul mb-0">
-          <li>
-            <i class="fa-li fa fa-check"></i>
-            Responsive Design, UX/UI Design</li>
-          <li>
-            <i class="fa-li fa fa-check"></i>
-            Base de données : phpMyAdmin &amp; MariaDB</li>
-          <li>
-            <i class="fa-li fa fa-check"></i>
-            Connaissances en réseaux &amp; Développement d'un VPN à distance</li>
-          <li>
-            <i class="fa-li fa fa-check"></i>
-            Algorithmique Python &amp; C</li>
-          <li>
-            <i class="fa-li fa fa-check"></i>
-            Méthodes de Communication professionnelle</li>
-          <li>
-            <i class="fa-li fa fa-check"></i>
-            Gestion de Projet Agile &amp; Scrum</li>
-        </ul>
-      </div>
-    </section>
-
-    <hr class="m-0">
+    <?php require_once("inc/skills.inc.php"); ?>
 
     <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="interests">
       <div class="w-100">

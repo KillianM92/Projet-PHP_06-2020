efa7fe70f3ea8553d78639b824db1d4395fd60f6
KillianM92 <killian.moutinard@ynov.com>
2020-05-18 01:06:38 +0200

    Insertion données rubrique "Centres d'intérêt"
    Requêtes effectuées dans le fichier "interest_sql.inc.php" et insertion des requêtes dans le fichier "interests.inc.php"

---
+++ data/interest_sql.inc.php
@@ -0,0 +1,13 @@
+<?php require("data/data.inc.php"); ?>
+
+<?php
+
+// Requêtes SQL pour l'id_interest 1
+
+$result1 = $pdo->query("SELECT description FROM interest WHERE id_interest = 1");
+$description1 = $result1->fetch(PDO::FETCH_OBJ); 
+
+$result2 = $pdo->query("SELECT description FROM interest WHERE id_interest = 2");
+$description2 = $result2->fetch(PDO::FETCH_OBJ); 
+
+?>
--- inc/interests.inc.php
+++ inc/interests.inc.php
@@ -1,8 +1,10 @@
+<?php require("data/interest_sql.inc.php"); ?>
+
 <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="interests">
     <div class="w-100">
         <h2 class="mb-5">Centres d'intérêt</h2>
-        <p>En dehors de mes études dans le milieu de l'informatique, je passe pratiquement tout mon temps libre à faire du sport dans le domaine du basketball et de la musculation. J'aime beaucoup passer du temps avec mes amis, sortir pour se balader ou bien aller à des concerts et festivals.</p>
-        <p class="mb-0">Lorsque je suis chez moi, je regarde beaucoup de films et de séries fantastiques et d'horreur mais je ne rate pas l'occasion de regarder des matchs de différents sports. Le reste de mon temps libre est consacré aux jeux vidéos en ligne avec mes amis  mais aussi à me renseigner et tenter de découvrir les nouvelles technologies.</p>
+        <p><?php echo $description1->description;?></p>
+        <p class="mb-0"><?php echo $description2->description;?></p>
     </div>
 </section>
 
